74bc6a38436dfc55a5be8fe95d2c35bfe1202e1b
Peter Jacobson <github email address>
2016-09-21 15:01:30 -0400

    add login routing for reader_controller

--- app/controllers/reader_controller.rb
+++ app/controllers/reader_controller.rb
@@ -32,4 +32,60 @@ class ReadersController < ApplicationController
   delete "/readers/:id/delete" do
     redirect "/readers"
   end
+
+
+
+    get '/readers/:slug' do
+    @reader = reader.find_by_slug(params[:slug])
+    erb :'readers/show'
+  end
+
+  get '/signup' do
+    if !logged_in?
+      erb :'readers/create_reader', locals: {message: "Please sign up before you sign in"}
+    else
+      redirect to :'/books/index'
+    end
+  end
+
+  post '/signup' do 
+    if params[:readername] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @reader = reader.new(:readername => params[:readername], :email => params[:email], :password => params[:password])
+      @reader.save
+      session[:reader_id] = @reader.id
+      redirect to :'/books/index'
+    end
+  end
+
+  get '/login' do 
+    if !logged_in?
+      erb :'readers/login'
+    else
+      redirect '/tweets'
+    end
+  end
+
+  post '/login' do
+    reader = reader.find_by(:readername => params[:readername])
+    if reader && reader.authenticate(params[:password])
+      session[:reader_id] = reader.id
+      redirect "/tweets"
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
+
+
 end
