946971ffd1556761224bd9b7d32225f2d39afd0f
Peter Jacobson <github email address>
2016-09-23 14:04:03 -0400

    more complexity signup application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+    register Sinatra::ActiveRecordExtension
+  set :views, Proc.new { File.join(root, "../views/") }
 
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    #set :public_folder, 'public'
+    #set :views, 'app/views'
     enable :sessions
     set :session_secret, "read_secret"
   end
@@ -15,24 +17,46 @@ class ApplicationController < Sinatra::Base
 
 
     get '/registrations/signup' do
+      if !logged_in?
     erb :'/registrations/signup'
+  else 
+    redirect to '/index'
   end
 
   post '/registrations' do
-    
-    redirect 'readers/show'
+     if params[:name] == "" || params[:email] == "" || params[:password] == ""
+       redirect to '/registrations/signup'
+     else
+      @reader = Reader.new(:name => params[:name], :email => params[:email], :password => params[:password])
+       @reader.save
+       session[:user_id] = @reader.id
+    redirect 'users/home'
   end
 
   get '/sessions/login' do
+    if !logged_in?
     erb :'sessions/login'
+  else
+    redirect '/books'
+  end
   end
 
   post '/sessions' do
-    redirect 'readers/show'
+     reader = Reader.find_by(:name => params[:name])
+    if reader && reader.authenticate(params[:password])
+      session[:user_id] = reader.id
+    redirect 'users/home'
+  else
+    redirect to '/registrations'
+  end
   end
 
   get '/sessions/logout' do 
     redirect '/'
+  end
+
+ get 'users/home' do
+    erb :'users/home'
   end
 
 
