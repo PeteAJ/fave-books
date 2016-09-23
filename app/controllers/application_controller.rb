681416008b255232ed7cc8e6c249327acd17c10f
Peter Jacobson <github email address>
2016-09-23 15:34:27 -0400

    reader controller changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,51 +16,16 @@ class ApplicationController < Sinatra::Base
   end
 
 
-    get '/registrations/signup' do
-      if !logged_in?
-    erb :'/registrations/signup'
-  else 
-    redirect to '/index'
-  end
 
-  post '/registrations' do
-     if params[:name] == "" || params[:email] == "" || params[:password] == ""
-       redirect to '/registrations/signup'
-     else
-      @reader = Reader.new(:name => params[:name], :email => params[:email], :password => params[:password])
-       @reader.save
-       session[:user_id] = @reader.id
-    redirect 'users/home'
-  end
-
-  get '/sessions/login' do
-    if !logged_in?
-    erb :'sessions/login'
-  else
-    redirect '/books'
-  end
-  end
-
-  post '/sessions' do
-     reader = Reader.find_by(:name => params[:name])
-    if reader && reader.authenticate(params[:password])
-      session[:user_id] = reader.id
-    redirect 'users/home'
-  else
-    redirect to '/registrations'
-  end
-  end
-
-  get '/sessions/logout' do 
-    redirect '/'
-  end
 
  get 'users/home' do
     erb :'users/home'
   end
 
 
-  helpers do
+
+
+ helpers do
     def logged_in?
       !!session[:user_id]
     end
